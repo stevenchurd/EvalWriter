f7058a1f92f8f8609d167452f0d71015cba2e035
Steven Hurd <steven.c.hurd@gmail.com>
2013-05-14 07:50:08 -0700

    Installer dll updates

--- install/createInstaller.bat
+++ install/createInstaller.bat
@@ -1,4 +1,4 @@
-copy dlls\*.dll packages\com.vendor.product\data\ /Y
+xcopy /S dlls\* packages\com.vendor.product\data\ /Y
 copy ..\build-EvalWriter-Desktop_Qt_5_0_2_MSVC2010_32bit-Release\release\EvalWriter.exe packages\com.vendor.product\data\ /Y
 copy ..\build-EvalWriter-Desktop_Qt_5_0_2_MSVC2010_32bit-Release\release\EvalWriter.exe.embed.manifest packages\com.vendor.product\data\ /Y
 C:\Qt\QtIFW-1.3.0\bin\binarycreator.exe --offline-only -c config\config.xml -p packages EvalWriterInstaller.exe
